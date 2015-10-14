adae7fa4fe2d4c58660b4aaa6f9fc2acac7e5e31
Velina <velina@localhost.localdomain>
2015-10-14 08:15:21 -0700

    Establish initial TCP connection

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+
+from functools import partial
+import socket
+import sys
 
 class InitScreen(FloatLayout):
 
@@ -23,19 +29,103 @@ class InitScreen(FloatLayout):
         self.add_widget(server_button)
 
         self.add_widget(Label(text='TCP Connection', size_hint=(.5, .05), pos_hint={'top':0.50, 'right':0.5}))
-        self.add_widget(Button(text='Open Connection', size_hint=(.22, .05), pos_hint={'top':0.40, 'right':0.25}))
-        self.add_widget(Button(text='Close Connection', size_hint=(.22, .05), pos_hint={'top':0.40, 'right':0.5}))
+        open_connection_button  = Button(text='Open Connection', size_hint=(.22, .05), pos_hint={'top':0.40, 'right':0.25})
+        close_connection_button = Button(text='Close Connection', size_hint=(.22, .05), pos_hint={'top':0.40, 'right':0.5})
+        self.add_widget(open_connection_button)
+        self.add_widget(close_connection_button)
         
         self.add_widget(Label(text='Data To Be Sent', size_hint=(.5, .05), pos_hint={'top':0.30, 'right':0.5}))
         self.data_to_send = TextInput(password=False, multiline=True, size_hint=(.46, .15), pos_hint={'top':0.25, 'right':0.5})
         self.add_widget(self.data_to_send)
-        self.add_widget(Button(text='Send Data', size_hint=(.46, .05), pos_hint={'top':0.07, 'right':0.5}))
+        send_data_button = Button(text='Send Data', size_hint=(.46, .05), pos_hint={'top':0.07, 'right':0.5}, disabled='true')
+        self.add_widget(send_data_button)
 
         self.add_widget(Label(text='Console', size_hint=(.4, .05), pos_hint={'top':0.95, 'right':0.95}))
         self.console = TextInput(password=False, multiline=True, size_hint=(.40, .8), pos_hint={'top':0.90, 'right':0.95})
         self.add_widget(self.console)
         self.add_widget(Button(text='Continue', size_hint=(.40, .05), pos_hint={'top':0.07, 'right':0.95}))
-        
+
+        self.server_button = server_button
+        self.send_data_button = send_data_button
+
+        # bind event handlers
+        open_connection_button.bind(on_press=partial(self.connectionPrompt))
+        close_connection_button.bind(on_press=self.closeConnection)
+        send_data_button.bind(on_press=self.sendData)
+
+    
+    def connectionPrompt(self, obj):
+        prompt    = BoxLayout(size=(250,250),orientation="vertical",spacing=20,padding=20)
+        self.mode = 'server' if self.server_button.state == 'down' else 'client'
+
+        if self.mode == 'client':
+            self.host_input = TextInput(size_hint=(.8,.15))
+            prompt.add_widget(Label(text='Host:',size_hint=(.7,.1)))
+            prompt.add_widget(self.host_input)
+
+        self.port_input = TextInput(size_hint=(.8,.15))    
+        prompt.add_widget(Label(text='Port:',size_hint=(.7,.1)))
+        prompt.add_widget(self.port_input)
+
+        connect_button = Button(text='Connect',size_hint=(.4,.1),pos_hint={'right':.7})
+        prompt.add_widget(connect_button)
+
+        popup = Popup(title='Connect',content=prompt, size_hint=(.5,.5)) 
+        self.connection_popup = popup
+        popup.open()
+
+        connect_button.bind(on_press=self.connect)
+
+
+    def connect(self, obj):
+        self.connection_popup.dismiss()
+
+        #todo: host and port input validation
+
+        if self.mode == 'client':
+            self.socket = self.clientConnect(self.host_input.text, int(self.port_input.text))
+        else:
+            self.socket = self.serverConnect(int(self.port_input.text))
+
+        #todo: mutual authentication here?
+
+        self.send_data_button.disabled = False   
+
+    def closeConnection(self, obj):
+        self.socket.close()
+        self.send_data_button.disabled = True
+        self.data_to_send.text = ''
+
+    def sendData(self, obj):
+        #todo: encryption here?
+
+        self.socket.sendall(self.data_to_send.text)
+        self.data_to_send.text = ''
+
+    def clientConnect(self, host, port):
+        # create the client socket
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # connect to given ip and port
+        try:
+            client_socket.connect((host, port))
+        except socket.error, msg:
+            print "Could not connect."
+            #todo: we may not want to actually exit the whole thing here
+            sys.exit(1)
+
+        return client_socket
+
+    def serverConnect(self, port):
+        # create the server socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # bind the socket to host and port
+        server_socket.bind((socket.gethostname(), port))
+        # listen for a connection
+        server_socket.listen(1)
+
+        sock, addr = server_socket.accept()
+        return sock   
+  
 
 class MyApp(App):
 
