82838c1c3b29839cd4b72743e498ebe6423d3cd2
Peter Zhang <peter9207@hotmail.com>
2015-10-21 15:16:55 -0700

    modified hmac

--- hmac_gen.py
+++ hmac_gen.py
@@ -1,14 +1,8 @@
 import hashlib
 import hmac
 
-class hmacGenerator:
 
+def genHmac(key, msg):
+	return hmac.new(key, msg, haslib.md5).hexdigest()
 
-    def __init__(self, key, msg):
-        self.key = key
-        self.msg = msg
-
-
-    def genHmac(self):
-        result = hmac.new(self.key, self.msg, hashlib.md5).hexdigest()
-        return result		
+	
